c8a943a08905d49e87087f760d78b96b2d33c948
yas-say <yasowant@live.com>
2023-03-17 12:08:48 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=1000, height=600)
+screen.title("Pingpong")
+screen.bgcolor("Black")
+screen.tracer(0)
+
+
+
+
+screen.exitonclick()
